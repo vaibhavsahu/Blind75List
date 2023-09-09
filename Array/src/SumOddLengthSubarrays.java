acd31282e09e218baf52cbe67870de292ccc83d2
vaibhavsahu <vaibh3jec@gmail.com>
2023-09-09 01:33:17 -0600

    1588. Sum of All Odd Length Subarrays

---
+++ Array/src/SumOddLengthSubarrays.java
@@ -0,0 +1,14 @@
+public class SumOddLengthSubarrays {
+    //https://leetcode.com/problems/sum-of-all-odd-length-subarrays/
+    public int sumOddLengthSubarrays(int[] arr) {
+        int n = arr.length, answer = 0;
+
+        for (int i = 0; i < n; ++i) {
+            int left = i, right = n - i - 1;
+            answer += arr[i] * (left / 2 + 1) * (right / 2 + 1);
+            answer += arr[i] * ((left + 1) / 2) * ((right + 1) / 2);
+        }
+
+        return answer;
+    }
+}
