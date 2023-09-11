19a2dc9bd854c7b0b4babbac9256a88e893bf065
vaibhavsahu <vaibh3jec@gmail.com>
2023-09-10 22:15:33 -0600

    1720. Decode XORed Array

---
+++ Array/src/DecodeXORedArray.java
@@ -0,0 +1,14 @@
+//https://leetcode.com/problems/decode-xored-array/
+public class DecodeXORedArray {
+    public int[] decode(int[] encoded, int first) {
+        int [] arr = new int[encoded.length+1];
+
+        arr[0] = first;
+
+        for(int i = 1; i <= encoded.length; i++){
+            arr[i] = arr[i-1] ^  encoded[i-1];
+        }
+
+        return arr;
+    }
+}
