4a4a357028bb933f3a30d639f6c7fcdebf3650c5
vaibhavsahu <vaibh3jec@gmail.com>
2023-09-10 22:44:50 -0600

    1528. Shuffle String

---
+++ Array/src/ShuffleString.java
@@ -0,0 +1,10 @@
+//https://leetcode.com/problems/shuffle-string/
+public class ShuffleString {
+    public String restoreString(String s, int[] indices) {
+        char[] ch = new char[s.length()];
+        for(int i=0;i<s.length();i++){
+            ch[indices[i]] = s.charAt(i);
+        }
+        return String.valueOf(ch);
+    }
+}
