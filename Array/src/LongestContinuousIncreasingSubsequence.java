73e231c161413457eba7bfd9a957655df8295a73
vaibhavsahu <vaibh3jec@gmail.com>
2023-09-21 19:07:20 -0600

    674. Longest Continuous Increasing Subsequence

---
+++ Array/src/LongestContinuousIncreasingSubsequence.java
@@ -0,0 +1,11 @@
+public class LongestContinuousIncreasingSubsequence {
+    public int findLengthOfLCIS(int[] nums) {
+        //https://leetcode.com/problems/longest-continuous-increasing-subsequence/
+        int ans = 0, anchor = 0;
+        for (int i = 0; i < nums.length; ++i) {
+            if (i > 0 && nums[i-1] >= nums[i]) anchor = i;
+            ans = Math.max(ans, i - anchor + 1);
+        }
+        return ans;
+    }
+}
