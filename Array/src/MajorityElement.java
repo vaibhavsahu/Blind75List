cec54337e5d335a8cd082d4e4aee822df5535b5a
vaibhavsahu <vaibh3jec@gmail.com>
2023-09-30 23:33:31 -0600

    169. Majority Element

---
+++ Array/src/MajorityElement.java
@@ -0,0 +1,23 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class MajorityElement {
+    //https://leetcode.com/problems/majority-element/editorial/
+    public int majorityElement(int[] nums) {
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for(int num: nums){
+            map.put(num, map.getOrDefault(num, 0)+1);
+        }
+
+        int n = nums.length;
+
+        for(Map.Entry<Integer, Integer> entry : map.entrySet()){
+            if(entry.getValue() > n/2){
+                return entry.getKey();
+            }
+        }
+
+        return -1;
+    }
+}
